cc12057092eb1027c7cb1b54d6cd353f832761ff
bloodskin <owner@bloodskin.com>
2023-03-22 00:09:43 -0400

    Update Forecast.cs

--- Forecast.cs
+++ Forecast.cs
@@ -1,16 +1,19 @@
 using Oxide.Core.Plugins;
 using Oxide.Core.Libraries.Covalence;
+using System.Linq;
 
 namespace Oxide.Plugins
 {
-    [Info("Forecast", "BloodSkin", "1.0.0")]
+    [Info("Forecast", "BloodSkin", "1.0.2")]
     class Forecast : CovalencePlugin
     {
         [Command("forecast")]
-        private void ForecastCommand(IPlayer player, string command, string[] args)
-        {
-            var report = ConsoleSystem.Run(ConsoleSystem.Option.Server.Quiet(), "weather.report");
-            player.Reply(report);
-        }
+private void ForecastCommand(IPlayer player, string command, string[] args)
+{
+    var report = ConsoleSystem.Run(ConsoleSystem.Option.Server.Quiet(), "weather.report");
+    var lines = report.Split('\n');
+    var firstLineParts = lines[0].Split('|');
+    player.Reply("<color=#9999ff>Todays Forecast:</color> " + firstLineParts[2].Trim());
+}
     }
 }
